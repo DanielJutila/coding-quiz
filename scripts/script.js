eba87dab8f2e29755bca12cac0e5063bfb3f15cf
Daniel Jutila <daniel.jutila@gmail.com>
2023-11-20 17:58:21 -0600

    Changed everything, Added a easy answer check and stopped timer bug from happening

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,80 @@
 var timer = document.querySelector(".timer");
-var questions = document.querySelector(".question");
-var startGame = document.querySelector(".start-game");
-
+var questionGame = document.getElementById("questions");
+var startGame = document.querySelector(".startGame");
+var answer = document.querySelectorAll(".answer");
+var qArray = [
+    document.querySelector(".q1"),
+    document.querySelector(".q2"),
+    document.querySelector(".q3"),
+    document.querySelector(".q4"),
+    document.querySelector(".q5"),
+    document.querySelector(".q6")
+];
+var correctButtons = document.querySelectorAll(".correct");
+var wrongButtons = document.querySelectorAll(".wrong");
 
 //Timer for quiz
 //Add seconds to here when answer is correct
-var secondsLeft = 20;
+var secondsLeft =30;
+var questionOrder = 0;
 
 //starts game
-startGame.addEventListener("click", function(){
+startGame.addEventListener("click", function () {
+    console.log("start game pressed");
+    secondsLeft = 30;
+    clearInterval(timeInterval);
     setTime();
-    questions.textContent = "test";
-    function setTime(){
-        var timeInterval = setInterval(function(){
+    startGame.setAttribute("style", "display:none;");
+    qArray[questionOrder].setAttribute("style", "display: block;");
+    timer.setAttribute("style", "display: inline;");
+
+    });
+
+function loopingThourgh(){
+    console.log("LOOPED");
+    answer.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            handleAnswer(event.target.classList.contains("correct"));
+        })
+    })
+};
+
+var timeInterval;
+
+function setTime() {
+        timeInterval = setInterval(function () {
             secondsLeft--;
             timer.textContent = secondsLeft;
-            //makes start game invisible after pressing
-            startGame.setAttribute("style", "display:none;")
-            if(secondsLeft === 0){
+            if (secondsLeft == 0) {
                 clearInterval(timeInterval);
-                //write code if time runs out
+                endGame();
             }
-        
-        }, 1000)
-        }
-
-})
-
-//Adds or subtracts time based on the answer
-function answerResults(answer){
-    if(answer){
-        secondsLeft += 5;
-    } else {
-        secondsLeft -= 5;
-    }
+        }, 1000);
 }
 
-var q1 = {
-    test: "apple",
-    test2: "orange"
+function handleAnswer(isCorrect) {
+    
+    qArray[questionOrder].setAttribute("style", "display: none;");
+    questionOrder++;
+    if (isCorrect) {
+        secondsLeft += 10;
+    } else {
+        secondsLeft -= 10;
+    }
+    if (questionOrder < qArray.length) {
+        qArray[questionOrder].setAttribute("style", "display: block;");
+    } else {
+        endGame();
+    }
 };
-var q2 = {
-    test: "apple",
-    test2: "orange"
+
+//end game function
+function endGame() {
+    console.log("end game function " + secondsLeft);
+    clearInterval(timeInterval);
+    questionOrder = 0;
+    timer.setAttribute("style", "display: none;");
+    startGame.setAttribute("style", "display: inline;");
 };
-var q3 = {
-    test: "apple",
-    test2: "orange"
-};
-var q4 = {
-    test: "apple",
-    test2: "orange"
-};
+
+loopingThourgh();
