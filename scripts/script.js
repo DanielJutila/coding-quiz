7b91e0a4bd0b53755a5b283e5e6cc72a68663ce3
Daniel Jutila <daniel.jutila@gmail.com>
2023-11-22 15:46:51 -0600

    changed quite a lot. Game is working though now just need to add questions

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-var timer = document.querySelector(".timer");
-var questionGame = document.getElementById("questions");
-var startGame = document.querySelector(".startGame");
-var answer = document.querySelectorAll(".answer");
+var timer = document.querySelector('.timer');
+var questionGame = document.getElementById('questions');
+var startGame = document.querySelector('.startGame');
+var answer = document.querySelectorAll('.answer');
 var qArray = [
-    document.querySelector(".q1"),
-    document.querySelector(".q2"),
-    document.querySelector(".q3"),
-    document.querySelector(".q4"),
-    document.querySelector(".q5"),
-    document.querySelector(".q6")
+    document.querySelector('.q1'),
+    document.querySelector('.q2'),
+    document.querySelector('.q3'),
+    document.querySelector('.q4'),
+    document.querySelector('.q5'),
+    document.querySelector('.q6')
 ];
-var correctButtons = document.querySelectorAll(".correct");
-var wrongButtons = document.querySelectorAll(".wrong");
+var correctButtons = document.querySelectorAll('.correct');
+var wrongButtons = document.querySelectorAll('.wrong');
 
 //Timer for quiz
 //Add seconds to here when answer is correct
@@ -19,22 +19,22 @@ var secondsLeft =30;
 var questionOrder = 0;
 
 //starts game
-startGame.addEventListener("click", function () {
-    console.log("start game pressed");
+startGame.addEventListener('click', function () {
+    console.log('start game pressed');
     secondsLeft = 30;
     clearInterval(timeInterval);
     setTime();
-    startGame.setAttribute("style", "display:none;");
-    qArray[questionOrder].setAttribute("style", "display: block;");
-    timer.setAttribute("style", "display: inline;");
+    startGame.setAttribute('style', 'display:none;');
+    qArray[questionOrder].setAttribute('style', 'display: block;');
+    timer.setAttribute('style', 'display: inline;');
 
     });
 
 function loopingThourgh(){
-    console.log("LOOPED");
+    console.log('LOOPED');
     answer.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-            handleAnswer(event.target.classList.contains("correct"));
+        button.addEventListener('click', function (event) {
+            handleAnswer(event.target.classList.contains('correct'));
         })
     })
 };
@@ -54,7 +54,7 @@ function setTime() {
 
 function handleAnswer(isCorrect) {
     
-    qArray[questionOrder].setAttribute("style", "display: none;");
+    qArray[questionOrder].setAttribute('style', 'display: none;');
     questionOrder++;
     if (isCorrect) {
         secondsLeft += 10;
@@ -62,19 +62,47 @@ function handleAnswer(isCorrect) {
         secondsLeft -= 10;
     }
     if (questionOrder < qArray.length) {
-        qArray[questionOrder].setAttribute("style", "display: block;");
+        qArray[questionOrder].setAttribute('style', 'display: block;');
     } else {
         endGame();
     }
 };
 
+var nameScore = document.querySelector('.score');
+var submitScore = document.querySelector('#submit');
+
 //end game function
 function endGame() {
-    console.log("end game function " + secondsLeft);
     clearInterval(timeInterval);
-    questionOrder = 0;
-    timer.setAttribute("style", "display: none;");
-    startGame.setAttribute("style", "display: inline;");
+    timer.setAttribute('style', 'display inline');
+    nameScore.setAttribute('style', 'display: block;');
+    timer.textContent = secondsLeft;
 };
 
+function highscore(event){
+    event.preventDefault();
+    //name inputed
+    var scoreName = document.querySelector('#name').value
+    player.name = scoreName;
+    player.score = secondsLeft;
+    localStorage.setItem('players', JSON.stringify(player));
+    startGame.setAttribute('style', 'display: inline;');
+    nameScore.setAttribute('style', 'display: none;');
+    questionOrder = 0;
+
+};
+
+var player = {
+    score: 0,
+    name: ""
+};
+
+submitScore.addEventListener('click', highscore)
+
 loopingThourgh();
+
+
+// clearInterval(timeInterval);
+// questionOrder = 0;
+// timer.setAttribute('style', 'display: none;');
+// startGame.setAttribute('style', 'display: inline;');
